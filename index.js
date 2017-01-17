5958082ccfd19ec7bcdba364bd68e2a9e40aba5b
Aleksandrenko <aleksandrenko@gmail.com>
2017-01-17 23:41:51 +0200

    Sample mutation with inmemory persistance

--- index.js
+++ index.js
@@ -2,96 +2,90 @@ var express = require('express');
 var graphqlHTTP = require('express-graphql');
 var { buildSchema } = require('graphql');
 
+var getId = () => require('crypto').randomBytes(10).toString('hex');
+
 // Construct a schema, using GraphQL schema language
 var schema = buildSchema(`
-  enum Episode { NEWHOPE, EMPIRE, JEDI }
-  
   input MessageInput {
     content: String
     author: String
   }
-  
+
   type Message {
     id: ID!
     content: String
     author: String
   }
-  
-  type Person {
-   id: ID!
-   name: String
-   homepage: String
-   friends: [Person]
-   interests: [Interest]
-  }
-  
-  type Activity {
-    id: ID!
-    name: String!
-    location: Location
-    type: Interest
-  }
-  
-  type Interest {
-    id: ID!
-    name: String!
-  }
-  
-  type Photo {
-    id: ID!
-    name: String!
-    url: String!
-    location: Location
-  }
-  
-  type Location {
-    id: ID!
-    name: String
-    latlng: String!
-  }
-  
+
   type Query {
-    users: [Person]
-    activities: [Activity]
-    interests: [Interest]
+    messages: [Message]
     getMessage(id: ID!): Message
   }
-  
+
   type Mutation {
     createMessage(input: MessageInput): Message
     updateMessage(id: ID!, input: MessageInput): Message
+    deleteMessage(id: ID!): String
   }
 `);
 
-// The root provides a resolver function for each API endpoint
+// If Message had any complex fields, we'd put them on this object.
+class Message {
+  constructor(id, {content, author}) {
+    this.id = id;
+    this.content = content;
+    this.author = author;
+  }
+}
+
+// Maps username to content
+var db = {};
+
 var root = {
-  users: () => [{
-    id: 0,
-    name: 'John Smith',
-    homepage: 'http://google.com',
-    friends: [],
-    interests: []
+  messages: function () {
+    return Object.keys(db).map((key) => {
+      var object = db[key];
+      object.id = key;
+
+      return object;
+    });
   },
-    {
-      id: 1,
-      name: 'Nikolay Aleksandrenko',
-      homepage: 'http://aleksandrenko.com',
-      friends: [],
-      interests: []
-    }],
-  activities: () => [],
-  interests: () => [],
-  getMessage: (id) => {}
-};
+  deleteMessage: function ({id}) {
+    if (!db[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+
+    db[id] = null;
+
+    return "";
+  },
+  getMessage: function ({id}) {
+    if (!db[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    return new Message(id, db[id]);
+  },
+  createMessage: function ({input}) {
+    var id = getId();
+    db[id] = input;
+    return new Message(id, input);
+  },
+  updateMessage: function ({id, input}) {
+    if (!db[id]) {
+      throw new Error('no message exists with id ' + id);
+    }
+    // This replaces all old data, but some apps might want partial update.
+    db[id] = input;
+    return new Message(id, input);
+  },
+}
 
 var app = express();
-
 app.use('/graphql', graphqlHTTP({
   schema: schema,
   rootValue: root,
-  graphiql: true
+  graphiql: true,
 }));
-
-app.listen(4000);
-
-console.log('Running a GraphQL API server at localhost:4000/graphql');
+app.listen(4000, () => {
+  console.log('Running a GraphQL API server at localhost:4000/graphql');
+});
